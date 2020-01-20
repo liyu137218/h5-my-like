d037a8cd758aefed47a1a559e002f3483d766113
陈伟 <chenwei9701@qq.com>
2020-01-20 23:47:10 +0800

    静态检查配置

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
 export default class App extends Vue {}
@@ -19,11 +16,8 @@ export default class App extends Vue {}
 
 <style lang="less">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width:100%;
+  overflow: hidden;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import './common/base.css';
+Vue.use(Vant);
 Vue.config.productionTip = false;
+
+import Vconsole from 'vconsole';
+
+if (window.location.href.indexOf('localhost') == -1) {
+  let vConsole: any = new Vconsole();
+  Vue.use(vConsole);
+}
 
 new Vue({
   router,
